d82cbcaab2d48f929fb9337d3e7e7502d1586c06
TIM FRENCH <tfrench15@gmail.com>
2018-02-17 23:33:07 -0800

    display bookshelves

---
+++ src/DisplayBooks.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Bookshelf from './Bookshelf'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+class DisplayBooks extends Component {
+	static propTypes = {
+		books: PropTypes.array.isRequired,
+		bookshelves: PropTypes.array.isRequired,
+		changeShelf: PropTypes.func.isRequired
+	}
+
+	render() {
+		{ books, bookshelves, changeShelf } = this.props 
+
+		return (
+			<div className="list-books">
+			  <div className="list-books-title">
+			    <h1>MyReads</h1>
+			  </div>
+			  <div className="list-books-content">
+			    <div>
+			      <Bookshelf
+			        key={bookshelf.id}
+			        books={books}
+			        id={bookshelf.id}
+			        name={bookshelf.name}
+			        changeShelf={changeShelf} 
+			      />
+			    </div>
+			  </div>
+			  <div className="open-search">
+			    <Link to="/search">Search for Books</Link>
+			  </div>
+			</div>
+		)
+	}
+}
+
+export default DisplayBooks
