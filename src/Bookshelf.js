ffb197313c39ed2208a12b5806b974b2512fb52d
TIM FRENCH <tfrench15@gmail.com>
2018-02-17 23:10:13 -0800

    clean up Bookshelf, fix export

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -34,12 +34,4 @@ class Bookshelf extends Component {
 	}
 }
 
-export default bookShelves
-
-
-<div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
+export default Bookshelf
