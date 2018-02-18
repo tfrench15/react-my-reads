9a134c5765181c6b6b5b2a6cb912d208e3c0fcaa
TIM FRENCH <tfrench15@gmail.com>
2018-02-18 00:39:18 -0800

    prep Search

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import { Link, Route } from 'react-router-dom'
-import escapeRegExp from 'escape-string-regexp'
+import PropTypes from 'prop-types'
 import sortBy from 'sort-by'
 import * as BooksAPI from './BooksAPI'
 
+/*
+
+The SearchForBooks Component controls the `/search` page.
+It allows a user to search for books and renders an array 
+of books that match that query.
+
+*/
 
 class SearchForBooks extends Component {
 	state = {
@@ -11,33 +18,91 @@ class SearchForBooks extends Component {
 		books: []
 	}
 
+	static propTypes = {
+		changeShelf: PropTypes.func.isRequired,
+		booksOnShelf: PropTypes.array.isRequired
+	}
+
+	getBooksOnShelf = (searchResults, booksOnShelf) => {
+		return searchResults.map((result) => {
+			let index = booksOnShelf.findIndex(book) => {
+				return book.id === result.id
+			})
+			if (index !== -1) {
+				result.shelf = booksOnShelf[index].shelf
+			} else {
+				result.shelf = 'none'
+			}
+			return result
+		}
+	}
+
 	updateQuery = (query) => {
-		return this.setState({
-			query: query.trim()
+		this.setState({
+			query: query.trim(),
+		}
+		if (query) {
+			BooksAPI.search(query).then((results) => {
+				if (!results.error) {
+					results = this.matchBooks(results, this.props.booksOnShelf)
+					this.setState({
+						books: results
+					})
+				} else {
+					this.setState({ books: [] })
+				}
+			})
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const results = this.matchBooks(this.state.books, nextProps.booksOnShelf)
+		this.setState({
+			books: results
 		})
 	}
 
 
 	render() {
+		const { books, query } = this.state
+		const { changeShelf } = this.props
 		return (
 			<div className="search-books">
-				<div className="search-books-bar">
-					<Link to="/" className="close-search">Close</Link>
-					<div className="search-books-input-wrapper">
-						<input 
-							type="text"
-							placeholder="Search by title or author"
-							value={this.state.query}
-							onChange={(event) => this.updateQuery(event.target.value)}
-						/>	
-					</div>
-				</div>
-					<div className="search-books-results">
-						<ol className="books-grid"></ol>
-					</div>
+			  <div className="search-books-bar">
+			    <Link to="/" className="close-search">Close</Link>
+			    <div className="search-books-input-wrapper">
+			      <input
+			        type="text"
+			        placeholder="Search by title or author"
+			        value={query}
+			        onChange={(event) => this.updateQuery(event.target.value)}
+			      />
+			    </div>
+			  </div>
+			  <div className="search-books-results">
+			    <ol className="books-grid"></ol>
+			  </div>
 			</div>
 		)
 	}
 }
 
 export default SearchForBooks
+
+<div className="search-books">
+			  <div className="search-books-bar">
+				<Link to="/" className="close-search">Close</Link>
+				<div className="search-books-input-wrapper">
+				  <input 
+					type="text"
+					placeholder="Search by title or author"
+					value={query}	
+					onChange={(event) => this.updateQuery(event.target.value)}
+				  />	
+				</div>
+			  </div>
+				  <div className="search-books-results">
+					  <ol className="books-grid"></ol>
+
+					</div>
+			</div>
